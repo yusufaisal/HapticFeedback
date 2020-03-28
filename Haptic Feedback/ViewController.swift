e705d64d2c2b6c1d33bf949219c2af8bbb46820c
Yusuf Faisal <yusuf@koinworks.com>
2020-03-28 09:58:59 +0700

    Initial Commit

---
+++ Haptic Feedback/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Haptic Feedback
+//
+//  Created by KoinWorks on 28/03/20.
+//  Copyright © 2020 iSal Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
